625dc76108d62e7fe469eab11df37349ab15427d
Alessandro <ale-remo@hotmail.com>
2016-11-10 09:11:24 -0300

    Fixing bug on updating the incorrect tab

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
-var url = '';
+var url = '', urlID;
 
 function redirectToMirror(classes) {
 	if (typeof classes !== 'undefined') {
 		if (classes.indexOf('hid') === -1) {
-			chrome.tabs.update(null, {url: url.replace('tube', 'pak')});
-		} 
+			chrome.tabs.update(urlID, {url: url.replace('tube', 'pak')});
+		}
 	}
 }
 
 chrome.tabs.onUpdated.addListener(function (tab) {
-    chrome.tabs.get(tab, function(tab_real) {
-    	url = tab_real.url;
-    	chrome.tabs.sendMessage(tab, {text: 'report_back'}, redirectToMirror);
-    });
+	chrome.tabs.get(tab, function(tab_real) {
+		url = tab_real.url;
+		urlID = tab_real.id;
+		chrome.tabs.sendMessage(tab, {text: 'report_back'}, redirectToMirror);
+	});
 });
