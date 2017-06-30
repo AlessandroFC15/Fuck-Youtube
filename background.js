207f14e56a0d9dffaf6d5ecaa8675607d531c147
Alessandro <ale-remo@hotmail.com>
2017-06-30 11:12:10 -0300

    Surprise, surprise motherfuckers, the king is back!

--- background.js
+++ background.js
@@ -1,9 +1,30 @@
-var url = '', urlID;
+var url = '', urlID, tabID;
+
+var getLinksFromYouPak = function(youtubeUrl) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", youtubeUrl.replace('tube', 'pak'), false);
+    xhr.send();
+
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(xhr.responseText, "text/html");
+
+    var sources = htmlDoc.getElementsByTagName('video')[0].children;
+
+    return Array.prototype.slice.call(sources).map(function(element) {
+        return element.src;
+    });
+};
 
 function redirectToMirror(classes) {
 	if (typeof classes !== 'undefined') {
 		if (classes.indexOf('hid') === -1) {
-			chrome.tabs.update(urlID, {url: url.replace('tube', 'pak')});
+			var links = getLinksFromYouPak(url);
+
+            console.log(links);
+
+            chrome.tabs.sendMessage(tabID, {text: 'update_link', link: links[2]}, null);
+            // chrome.tabs.update(urlID, {url: links[2]});
 		}
 	}
 }
@@ -12,6 +33,9 @@ chrome.tabs.onUpdated.addListener(function (tab) {
 	chrome.tabs.get(tab, function(tab_real) {
 		url = tab_real.url;
 		urlID = tab_real.id;
+
+        tabID = tab_real.id;
+
 		chrome.tabs.sendMessage(tab, {text: 'report_back'}, redirectToMirror);
 	});
 });
--- content.js
+++ content.js
@@ -1,6 +1,30 @@
 // Listen for messages
+var warning;
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    console.log(msg);
+    console.log(sender);
+    console.log('-------------');
+
     if (msg.text === 'report_back') {
+        var classes = document.getElementById('player-unavailable').className;
+
+        if (typeof classes !== 'undefined') {
+            if (classes.indexOf('hid') === -1) {
+                warning = document.createElement('h1');
+                warning.className = 'message';
+                warning.style.textAlign = 'center';
+                warning.innerHTML = "You know what? Fuck Youtube. Wait a second, we're loading a mirror for you...";
+
+                var content = document.getElementById('player-unavailable').getElementsByClassName('content')[0]
+
+                content.appendChild(document.createElement('br'));
+                content.appendChild(warning);
+            }
+        }
+
         sendResponse(document.getElementById('player-unavailable').className);
+    } else if (msg.text === 'update_link') {
+        warning.innerHTML = '<a href="'  + msg.link +'">Your mirror is ready, click here to watch it!</a>'
     }
 });
