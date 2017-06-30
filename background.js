a434d857d5c9ba997e00b15c656b125c02bffb01
AlessandroFC15 <ale-remo@hotmail.com>
2017-06-30 17:34:00 -0300

    Got a working extension now
    I've put everything into this, I swear to God I've given up everything to pursue this. I am very happy and grateful to be where I am at.

--- background.js
+++ background.js
@@ -1,41 +1,44 @@
 var url = '', urlID, tabID;
 
-var getLinksFromYouPak = function(youtubeUrl) {
+var getHTMLDocumentFromUrl = function (url) {
     var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", youtubeUrl.replace('tube', 'pak'), false);
+    xhr.open("GET", url, false);
     xhr.send();
 
-    var parser = new DOMParser();
-    var htmlDoc = parser.parseFromString(xhr.responseText, "text/html");
+    return new DOMParser().parseFromString(xhr.responseText, "text/html");
+};
+
+var getLinksFromYouPak = function (youtubeUrl) {
+    var youPakUrl = youtubeUrl.replace('tube', 'pak');
+
+    var htmlDoc = getHTMLDocumentFromUrl(youPakUrl);
 
     var sources = htmlDoc.getElementsByTagName('video')[0].children;
 
-    return Array.prototype.slice.call(sources).map(function(element) {
+    return Array.prototype.slice.call(sources).map(function (element) {
         return element.src;
     });
 };
 
-function redirectToMirror(classes) {
-	if (typeof classes !== 'undefined') {
-		if (classes.indexOf('hid') === -1) {
-			var links = getLinksFromYouPak(url);
-
-            console.log(links);
+var redirectToMirror = function (classes) {
+    if (typeof classes !== 'undefined') {
+        if (classes.indexOf('hid') === -1) {
+            var links = getLinksFromYouPak(url);
 
             chrome.tabs.sendMessage(tabID, {text: 'update_link', link: links[2]}, null);
-            // chrome.tabs.update(urlID, {url: links[2]});
-		}
-	}
-}
+        }
+    }
+};
 
-chrome.tabs.onUpdated.addListener(function (tab) {
-	chrome.tabs.get(tab, function(tab_real) {
-		url = tab_real.url;
-		urlID = tab_real.id;
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status && changeInfo.status == 'complete') {
+        chrome.tabs.get(tabId, function (tab_real) {
+            url = tab_real.url;
+            urlID = tab_real.id;
 
-        tabID = tab_real.id;
+            tabID = tab_real.id;
 
-		chrome.tabs.sendMessage(tab, {text: 'report_back'}, redirectToMirror);
-	});
+            chrome.tabs.sendMessage(tabId, {text: 'report_back'}, redirectToMirror);
+        });
+    }
 });
--- content.js
+++ content.js
@@ -1,11 +1,16 @@
 // Listen for messages
 var warning;
 
+var hideWarningVideoUnavailable = function () {
+    var divPlayerUnavailable = document.getElementById('player-unavailable');
+    divPlayerUnavailable.style.display = 'none';
+};
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     console.log(msg);
     console.log(sender);
     console.log('-------------');
-
+    
     if (msg.text === 'report_back') {
         var classes = document.getElementById('player-unavailable').className;
 
@@ -16,7 +21,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
                 warning.style.textAlign = 'center';
                 warning.innerHTML = "You know what? Fuck Youtube. Wait a second, we're loading a mirror for you...";
 
-                var content = document.getElementById('player-unavailable').getElementsByClassName('content')[0]
+                var content = document.getElementById('player-unavailable').getElementsByClassName('content')[0];
 
                 content.appendChild(document.createElement('br'));
                 content.appendChild(warning);
@@ -25,6 +30,22 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 
         sendResponse(document.getElementById('player-unavailable').className);
     } else if (msg.text === 'update_link') {
-        warning.innerHTML = '<a href="'  + msg.link +'">Your mirror is ready, click here to watch it!</a>'
+        hideWarningVideoUnavailable();
+        
+        var divPlayerAPI = document.getElementById('player-api');
+        divPlayerAPI.classList.remove('off-screen-target');
+
+        var videoTag = document.createElement('video');
+        videoTag.controls = true;
+        videoTag.autoplay = true;
+        videoTag.name = "media";
+        videoTag.style.width = '100%';
+
+        var srcTag = document.createElement('source');
+        srcTag.src = msg.link;
+        srcTag.type = 'video/mp4';
+
+        videoTag.appendChild(srcTag);
+        divPlayerAPI.appendChild(videoTag);
     }
 });
