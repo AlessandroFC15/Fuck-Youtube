2470aff1fb8bfe3bd02230fac18728495d4f1356
AlessandroFC15 <ale-remo@hotmail.com>
2017-06-30 23:16:35 -0300

    Refactoring done!
    I'm good, I'm good, I'm great

--- content.js
+++ content.js
@@ -1,51 +1,87 @@
-// Listen for messages
-var warning;
+"use strict";
 
-var hideWarningVideoUnavailable = function () {
-    var divPlayerUnavailable = document.getElementById('player-unavailable');
-    divPlayerUnavailable.style.display = 'none';
+var main = function () {
+    var url = window.location.toString();
+
+    if (isYoutubeLink(url)) {
+        if (isVideoUnavailable()) {
+            showLoadingFeedback();
+
+            var links = getLinksFromYouPak(url);
+
+            hideWarningVideoUnavailable();
+
+            createVideoFrame(links[2]);
+        }
+    }
 };
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    console.log(msg);
-    console.log(sender);
-    console.log('-------------');
-    
-    if (msg.text === 'report_back') {
-        var classes = document.getElementById('player-unavailable').className;
+var isYoutubeLink = function (url) {
+    return /(\w*)youtube.com\/watch\?(\w*)/.test(url);
+};
 
-        if (typeof classes !== 'undefined') {
-            if (classes.indexOf('hid') === -1) {
-                warning = document.createElement('h1');
-                warning.className = 'message';
-                warning.style.textAlign = 'center';
-                warning.innerHTML = "You know what? Fuck Youtube. Wait a second, we're loading a mirror for you...";
+var isVideoUnavailable = function () {
+    var divPlayerUnavailable = document.getElementById("player-unavailable");
 
-                var content = document.getElementById('player-unavailable').getElementsByClassName('content')[0];
+    // Para que o vídeo seja considerado indisponível, é necessário que a div acima exista e que ela não a possua
+    // classe "hid", visto que esta classe tem como função esconder os elementos.
+    return divPlayerUnavailable !== undefined && divPlayerUnavailable.className.indexOf("hid") === -1;
+};
 
-                content.appendChild(document.createElement('br'));
-                content.appendChild(warning);
-            }
-        }
+var showLoadingFeedback = function () {
+    var warning = document.createElement("h1");
+    warning.className = "message";
+    warning.style.textAlign = "center";
+    warning.innerHTML = "You know what? Fuck Youtube. Wait a second, we're loading a mirror for you...";
 
-        sendResponse(document.getElementById('player-unavailable').className);
-    } else if (msg.text === 'update_link') {
-        hideWarningVideoUnavailable();
-        
-        var divPlayerAPI = document.getElementById('player-api');
-        divPlayerAPI.classList.remove('off-screen-target');
+    var content = document.getElementById("player-unavailable").getElementsByClassName("content")[0];
 
-        var videoTag = document.createElement('video');
-        videoTag.controls = true;
-        videoTag.autoplay = true;
-        videoTag.name = "media";
-        videoTag.style.width = '100%';
+    content.appendChild(document.createElement("br"));
+    content.appendChild(warning);
+};
 
-        var srcTag = document.createElement('source');
-        srcTag.src = msg.link;
-        srcTag.type = 'video/mp4';
+var getLinksFromYouPak = function (youtubeUrl) {
+    var youPakUrl = youtubeUrl.replace("tube", "pak");
 
-        videoTag.appendChild(srcTag);
-        divPlayerAPI.appendChild(videoTag);
-    }
-});
+    var htmlDoc = getHTMLDocumentFromUrl(youPakUrl);
+
+    var sources = htmlDoc.getElementsByTagName("video")[0].children;
+
+    return Array.prototype.slice.call(sources).map(function (element) {
+        return element.src;
+    });
+};
+
+var getHTMLDocumentFromUrl = function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send();
+
+    return new DOMParser().parseFromString(xhr.responseText, "text/html");
+};
+
+var hideWarningVideoUnavailable = function () {
+    var divPlayerUnavailable = document.getElementById("player-unavailable");
+    divPlayerUnavailable.style.display = "none";
+};
+
+var createVideoFrame = function (link) {
+    var divPlayerAPI = document.getElementById("player-api");
+    // This shows the previously hidden player holder.
+    divPlayerAPI.classList.remove("off-screen-target");
+
+    var videoTag = document.createElement("video");
+    videoTag.controls = true;
+    videoTag.autoplay = true;
+    videoTag.name = "media";
+    videoTag.style.width = "100%";
+
+    var srcTag = document.createElement("source");
+    srcTag.src = link;
+    srcTag.type = "video/mp4";
+
+    videoTag.appendChild(srcTag);
+    divPlayerAPI.appendChild(videoTag);
+};
+
+main();
