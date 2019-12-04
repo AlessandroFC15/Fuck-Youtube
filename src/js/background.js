dce44e0daede02f30efd6df6ed55ed1c4b873eb6
Alessandrø <alessandro96fc@gmail.com>
2019-12-04 16:29:49 -0300

    Send extension version header to API request

--- src/js/background.js
+++ src/js/background.js
@@ -1,12 +1,15 @@
 import "babel-polyfill";
 import axios from "axios";
 
+import { version } from "../manifest";
 import { mirrorApiUrl } from "./config";
+
+const requestConfig = { headers: { source: `extension@${version}`}};
 
 chrome.runtime.onMessage.addListener(
      ({ contentScriptQuery, videoId }, sender, sendResponse) => {
         if (contentScriptQuery === "queryVideoSrc") {
-            axios.get(`${mirrorApiUrl}/getVideoSrc?youtubeVideoId=${videoId}`)
+            axios.get(`${mirrorApiUrl}/getVideoSrc?youtubeVideoId=${videoId}`, requestConfig)
                 .then(res => sendResponse(res.data.url))
                 .catch(err => console.log(err) || sendResponse(null))
         }
